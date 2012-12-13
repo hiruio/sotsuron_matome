4463e18caadb3f87eede41a2cf9e9864e0bd5739
Takashi Watanabe <yamanokama@gmail.com>
2012-12-13 22:37:21 +0900

    indexの不具合修正

--- index.php
+++ index.php
@@ -5,23 +5,23 @@
 <body>
 <h1>14期生_卒論まとめ</h1>
 <a href="http://www.miyazaki-seminar.net/wiki/index.php?%E5%8D%92%E8%AB%96%2014th">マニュアル</a>
-<form action="sotsuron_functions.php">
+<form action="">
 <h2>卒論をさがす</h2>
 <input type="text"><input type="submit" value="検索"><br>
-<h2>こいつの卒論を読む</h2>
+<h2>卒論を読む</h2>
 <ul>
 <?php
 $xmls = scandir('./xml/');
 foreach($xmls as $xml){
-	if($xml == "." or $xml == ".."){
+	if($xml == "." or $xml == ".." or $xml == "images"){
 	}else{
 		$xml_file = './xml/' . $xml;
-		print '<li><a href="./mirudake.php?xml=' . $xml . '">' . get_author($xml_file) . '</a></li>';
+		print '<li><a href="./mirudake.php?xml=' . $xml . '&tilt=none">' . get_author($xml_file) . '</a></li>';
 	}
 }
 ?>
 </ul>
-<h2>俺の卒論をアップロードする</h2>
+<h2>卒論をアップロードする</h2>
 <a href="upload.html">アップロード用ページ</a><br>
 もしくは、自分でFTPクライアントを使ってアップロードしてください
 </form>
